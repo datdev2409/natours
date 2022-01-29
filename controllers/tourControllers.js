5910ebb61a74ca7386f612418e9185a54491b219
Cong Dat <datdev2409@gmail.com>
2022-01-29 09:52:42 +0700

    feat add CRUD for api

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -1,28 +1,56 @@
 const Tour = require('../models/tourModel');
 
+function createResponse(status, data, message = '') {
+	return { status, data, message };
+}
+
 class TourController {
 	async getAllTours(req, res) {
-		const query = await Tour.find({});
-		res.json(query);
-		// res.send('get all tours');
+		try {
+			const query = await Tour.find({});
+			res.status(200).json(createResponse('success', { tour: query }));
+		} catch (error) {
+			res.status(404).json(createResponse('fail', '', error));
+		}
 	}
 
 	async createTour(req, res) {
-		Tour.create(req.body);
-		res.send('create tour');
+		try {
+			const newTour = await Tour.create(req.body);
+			res.status(200).json(createResponse('success', { tour: newTour }, ''));
+		} catch (error) {
+			res.status(404).json(createResponse('fail', '', error));
+		}
 	}
 
-	getTour(req, res) {
-		const id = req.params.id;
-		res.send('get tour');
+	async getTour(req, res) {
+		try {
+			const id = req.params.id;
+			const tour = await Tour.findById(id);
+			res.status(200).json(createResponse('success', { tour }, ''));
+		} catch (error) {
+			res.status(404).json(createResponse('fail', '', 'Invalid tour id'));
+		}
 	}
 
-	updateTour(req, res) {
-		res.send('update tour');
+	async updateTour(req, res) {
+		try {
+			const id = req.params.id;
+			const updatedTour = await Tour.findByIdAndUpdate(id, req.body);
+			res.status(200).json(createResponse('success', { tour: updatedTour }));
+		} catch (error) {
+			res.status(404).json(createResponse('fail', '', error));
+		}
 	}
 
-	deleteTour(req, res) {
-		res.send('delete tour');
+	async deleteTour(req, res) {
+		try {
+			const id = req.params.id;
+			const deletedTour = await Tour.findByIdAndDelete(id);
+			res.status(204).json(createResponse('success', null));
+		} catch (error) {
+			res.status(404).json(createResponse('fail', '', error));
+		}
 	}
 }
 
