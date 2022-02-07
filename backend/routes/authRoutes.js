7ec325a64337acda5a70ab6895152af1819f2b9c
Cong Dat <datdev2409@gmail.com>
2022-02-07 23:43:13 +0700

    refactor

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,8 +3,6 @@ const authController = require('../controllers/authController')
 
 router.post('/register', authController.register)
 router.post('/login', authController.login)
-
-// Sent email to user, with reset token
 router.post('/password_forgot', authController.forgotPassword)
 router.post('/password_reset/:token', authController.resetPassword)
 
